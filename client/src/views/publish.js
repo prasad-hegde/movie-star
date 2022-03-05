7c47bb95fde2ba44add4a0666cfb8a0c5d673a3c
Prasad Hegde <phegde@iu.edu>
2022-03-05 17:42:13 -0500

    publish page updated

--- client/src/views/publish.js
+++ client/src/views/publish.js
@@ -1,3 +1,46 @@
+import AutoComplete from "../components/AutoComplete";
+import styled from "styled-components";
+import { dimension, languages, top100Films } from "../mock";
+import TextArea from "../components/TextArea";
+import Chips from "../components/Chips";
+import { FormElement } from "../commonStyle";
+import Button from "../components/Button";
+
+const RowFlex = styled.div`
+display: flex;
+flex-direction: column;
+`
+const Container = styled(RowFlex)`
+margin: 0px 3.5rem;
+width: 40%;
+`
+const FormWrap = styled(RowFlex)`
+margin-bottom: 10rem;
+`
+const Title = styled.div`
+font-size: 24px;
+font-weight: 700;
+margin: 2rem 0;
+`
 export default function Publish() {
-    return(<h1>Publish</h1>)
+
+    return (
+        <Container>
+            <Title>Publish Movie</Title>
+            <FormWrap>
+                <TextArea label="Movie Name" required />
+                <TextArea label="Synopsis" multiline required/>
+                <Chips options={languages} />
+                <Chips options={dimension} />
+                <TextArea label="Run Time" format="hhmm" required/>
+                <AutoComplete label="Movies" options={top100Films} multiple={true} />
+                <TextArea label="Genre" multiline />
+                <AutoComplete label="Locations" options={top100Films} multiple={true} />
+                <AutoComplete label="Theatres" options={top100Films} multiple={true} />
+                <FormElement justifyContent="flex-end">
+                    <Button label="Publish" position="end" onClick={()=>''}/>
+                </FormElement>
+            </FormWrap>
+        </Container>
+    )
 }
