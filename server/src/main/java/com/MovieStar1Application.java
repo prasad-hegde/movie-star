112432c438ae4218f0dc86a2082fb64c52c893f6
unknown <asarnoba@iu.edu>
2022-03-26 16:13:59 -0400

    added support for cors config

--- server/src/main/java/com/MovieStar1Application.java
+++ server/src/main/java/com/MovieStar1Application.java
@@ -2,12 +2,27 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MovieStar1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieStar1Application.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				
+				registry.addMapping("users").allowedOrigins("https://webserver-movie-star.herokuapp.com");
+				registry.addMapping("movies").allowedOrigins("https://webserver-movie-star.herokuapp.com");
+			}
+		};
 	}
 
 }
