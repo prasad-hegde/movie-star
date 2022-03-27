607b6d41b7f1c7698896e749fa0de9ed2daf094f
unknown <asarnoba@iu.edu>
2022-03-27 14:31:53 -0400

    updated global config

--- server/src/main/java/com/MovieStar1Application.java
+++ server/src/main/java/com/MovieStar1Application.java
@@ -19,7 +19,7 @@ public class MovieStar1Application {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				
-				registry.addMapping("/*").allowedOrigins("*");
+				registry.addMapping("/**");
 			}
 		};
 	}
