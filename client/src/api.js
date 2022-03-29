69bb8e0d7b7bdf7c3e4e2e4fb98e40812db29529
Prasad Hegde <phegde@iu.edu>
2022-03-29 12:43:40 -0400

    add apis

--- client/src/api.js
+++ client/src/api.js
@@ -1,1 +1,11 @@
-const domain = 'https://movie-star-server.herokuapp.com/movie/all';
+import axios from "axios";
+
+const domain = 'https://movie-star-server.herokuapp.com';
+
+export const publishMovie = (payload) => axios.post(`${domain}/movies/publish`, payload);
+
+export const getAllMovies = () => `${domain}/movies/all`;
+
+export const searchMovie = (search) => axios.get(`${domain}/movies/findAll?keyword=${search}`);
+
+export const getAmovie = (id) => `${domain}/movies/${id}`;
