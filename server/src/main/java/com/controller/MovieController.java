814beec998f85ea456d8b851ac36a4408f831560
unknown <asarnoba@iu.edu>
2022-03-28 20:22:18 -0400

    changes to movie controller

--- server/src/main/java/com/controller/MovieController.java
+++ server/src/main/java/com/controller/MovieController.java
@@ -1,18 +1,29 @@
 package com.controller;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.model.Movie;
 import com.service.MovieService;
@@ -22,38 +33,35 @@ import com.service.Status;
 @RequestMapping("/movies")
 @CrossOrigin
 public class MovieController {
-	
+
+
 	@Autowired
 	private MovieService movieService;
-	
-	//publish movies
-	@PostMapping(value="/publish")
-	@CrossOrigin
-	public Status addMovie(@Valid @RequestBody Movie movie) throws IOException 
-	{ 
+
+	// publish movies
+	@PostMapping(value = "/publish")
+	public Status addMovie(@Valid @RequestBody Movie movie) throws IOException {
 		System.out.print("inside publish movie");
 		movieService.saveMovie(movie);
-		return Status.SUCCESS; 
+		return Status.SUCCESS;
 	}
-	
-	
-	//find movies using keyword
+
+
+	// find movies using keyword
 	@GetMapping("/findAll")
-	public List<Movie> findEvents(Model model, @Param("keyword") String keyword){
-	    List<Movie> listMovies = movieService.findAllMovies(keyword);
-	   
-		
-		 model.addAttribute("listMovies", listMovies);
-		 model.addAttribute("keyword",keyword);
-		 return listMovies;
-		 
+	public List<Movie> findMovies(Model model, @Param("keyword") String keyword) {
+		List<Movie> listMovies = movieService.findAllMovies(keyword);
+
+		model.addAttribute("listMovies", listMovies);
+		model.addAttribute("keyword", keyword);
+		return listMovies;
+
 	}
-	
-	//get all movies
+
+	// get all movies
 	@GetMapping(value = "/all")
-	@CrossOrigin
-    	public ResponseEntity getAllEvents() {
-        	return ResponseEntity.ok(movieService.listAllMovies());
-    	}
-	
+	public ResponseEntity getAllEvents() {
+		return ResponseEntity.ok(movieService.listAllMovies());
+	}
+
 }
